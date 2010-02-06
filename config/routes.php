a58b978a7789adce648fe034cebaae39b27c59f1
Paul Herron <contact@paulherron.net>
2010-02-06 18:31:06 +0000

    Removed modifications for CakePHP integration.

--- config/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
-Router::connect('/admin', array('admin' => true, 'plugin' => 'users', 'controller' => 'users', 'action' => 'home'));
-
-// These routes should work automatically, as the controller name is the same as the plugin name. For some reason it doesn't, though, so needs to be forced. 
-Router::connect('/admin/users/:action/*', array('plugin' => 'users', 'admin' => true, 'controller' => 'users'));
-Router::connect('/admin/articles/:action/*', array('plugin' => 'articles', 'admin' => true, 'controller' => 'articles'));
-?>
--- views/elements/flash.ctp
+++
@@ -1,4 +0,0 @@
-<div id="messages">
-	<?php if ($session->check('Message.flash')) { $session->flash(); } ?> 
-	<?php if ($session->check('Message.auth')) { $session->flash('auth'); } ?> 
-</div>
